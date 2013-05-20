aca5e7f8f780181bc94fb9a6257fba47463bed19
FruitieX <fruitiex@gmail.com>
2013-05-21 02:30:12 +0300

    fix bug with symlinking of zshrc

--- firstrun.sh
+++ firstrun.sh
@@ -1,4 +1,7 @@
 #!/bin/zsh
 
-# fetch oh-my-zsh configs, run this before the symlink.sh script
+# fetch oh-my-zsh configs
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+
+# remove oh-my-zsh default .zshrc (if a .zshrc existed before installing oh-my-zsh it was backed up)
+rm ~/.zshrc
