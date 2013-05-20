e88f89edc4a2fe077051a923df9edeced5ce4e1d
FruitieX <fruitiex@gmail.com>
2013-05-21 02:34:46 +0300

    powerline theme for oh-my-zsh, add pentadactylrc and the new theme to symlink.sh

--- symlink.sh
+++ symlink.sh
@@ -4,8 +4,20 @@ for FILE in .[^.git]*; do
 	if [ ! -f "$HOME/$FILE" ] && [ ! -d "$HOME/$FILE" ];
 	then
 		echo "$FILE => $HOME/$FILE"
-		ln -sr $FILE $HOME/$FILE
+		ln -sr "$FILE" "$HOME/$FILE"
 	fi
 done
+
+# pentadactyl does not like unix dot notation :p
+if [ ! -f "$HOME/_pentadactylrc" ];
+then
+	ln -sr "_pentadactylrc" "$HOME/_pentadactylrc"
+fi
+
+# custom oh-my-zsh theme
+if [ ! -f "$HOME/.oh-my-zsh/themes/powerline.zsh-theme" ];
+then
+	ln -sr "powerline.zsh-theme" "$HOME/.oh-my-zsh/themes/powerline.zsh-theme"
+fi
 
 echo "done!"
