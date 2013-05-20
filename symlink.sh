5881ab0828266c36b30a91f225c50d6867b0f772
FruitieX <fruitiex@gmail.com>
2013-05-21 01:22:19 +0300

    symlink script

---
+++ symlink.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for FILE in $(find -maxdepth 1 -type f | grep -v "symlink.sh"); do
+	FILE=$(basename $FILE)
+	if [ ! -f "$HOME/$FILE" ];
+	then
+		echo "$FILE => $HOME/$FILE"
+		#ln -sr $FILE $HOME/$FILE
+	fi
+done
+
+echo "done!"
