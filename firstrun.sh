71c196f6afeee527e373312bb0373657609b1e3b
FruitieX <fruitiex@gmail.com>
2013-05-26 01:12:03 +0300

    git submodule init/update commands into firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -5,3 +5,6 @@ curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | 
 
 # remove oh-my-zsh default .zshrc (if a .zshrc existed before installing oh-my-zsh it was backed up)
 rm ~/.zshrc
+
+git submodule init
+git submodule update
