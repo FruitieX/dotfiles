5965fc33a0a348df3136147c2faa3ab0fb27e77c
Rasmus Eskola <fruitiex@gmail.com>
2013-07-11 21:32:40 +0300

    dwb take 2

--- firstrun.sh
+++ firstrun.sh
@@ -3,6 +3,9 @@
 # fetch oh-my-zsh configs
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 
+mkdir -p ~/.config/dwb
+curl -L https://easylist-downloads.adblockplus.org/easylist.txt > ~/.config/dwb/
+
 # remove oh-my-zsh default .zshrc (if a .zshrc existed before installing oh-my-zsh it was backed up)
 rm ~/.zshrc
 
