11bc743b9ce94b275daadc0bd5d0d6b159657c4d
Rasmus Eskola <fruitiex@gmail.com>
2013-07-12 17:58:13 +0300

    WIP: status bar

--- .i3status.sh
+++ .i3status.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-i3status | while :
+echo '{ "version": 1 }'
+echo '['
+echo '[]'
+
+while :
 do
-    read line
-    id=$(xprop -root | awk '/_NET_ACTIVE_WINDOW\(WINDOW\)/{print $NF}')
-    name=$(xprop -id $id | awk '/_NET_WM_NAME/{$1=$2="";print}' | cut -d'"' -f2)
-    echo "$name | $line" || exit 1
+    #id=$(xprop -root | awk '/_NET_ACTIVE_WINDOW\(WINDOW\)/{print $NF}')
+    #name=$(xprop -id $id | awk '/_NET_WM_NAME/{$1=$2="";print}' | cut -d'"' -f2)
+	date=$(date)
+	echo ",[{\"name\":\"time\",\"full_text\":\"$(date)\"}]"
+	sleep 1
 done
