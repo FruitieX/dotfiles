782d9614cf6518296ea402aea91741615ed487bb
Rasmus Eskola <fruitiex@gmail.com>
2013-07-26 15:26:34 +0300

    clean up and simplify symlink script

--- symlink.sh
+++ symlink.sh
@@ -1,23 +1,22 @@
 #!/bin/zsh
 
-# symlink individual files from this array
-FILES=( "bin" ".config" ".oh-my-zsh" ".synergy.conf" ".tmux.conf" ".urlview" ".vimrc" ".weechat" ".Xdefaults" ".xinitrc" ".Xmodmap" ".zshrc" )
-# symlink entire directories from this array
-DIRS=( ".vim" ".i3" )
+# recursively symlink all files from paths in this array
+FILES=( bin .config .i3 .oh-my-zsh .synergy.conf .tmux.conf .urlview .vim* .weechat .Xdefaults .xinitrc .Xmodmap .zshrc )
 
 # cd to root of git repo
 cd $(dirname $0)
 CWD=$(pwd)
 
+# dry run mode enabled?
+if [[ $1 == '-d' ]]; then
+	DRYRUN=echo
+else
+	DRYRUN=
+fi
+
 echo "WARNING! this script will backup the following files and put symlinks in their place:"
 for FILE in $(find $FILES -type f); do
 	echo "$HOME/$FILE"
-done
-echo
-
-echo "WARNING! this script will backup the following directories and put symlinks in their place:"
-for DIR in $DIRS; do
-	echo "$HOME/$DIR"
 done
 echo
 
@@ -36,36 +35,20 @@ for FILE in $(find $FILES -type f); do
 
 	# create parent directies if they do not exist
 	if [ ! -d $(dirname "$HOME/$FILE") ]; then
-		mkdir -p $(dirname "$HOME/$FILE")
+		$DRYRUN mkdir -p $(dirname "$HOME/$FILE")
 	fi
 
 	# if file is a symlink, remove it and later make a new symlink
 	if [ -L "$HOME/$FILE" ]; then
-		rm "$HOME/$FILE"
+		$DRYRUN rm "$HOME/$FILE"
 	# elif file already exists but is not backed up, make a backup
 	elif [ -f "$HOME/$FILE" -a ! -f "$HOME/$FILE.bak" ]; then
 		echo "backed up $HOME/$FILE => $HOME/$FILE.bak"
-		mv "$HOME/$FILE" "$HOME/$FILE.bak"
+		$DRYRUN mv "$HOME/$FILE" "$HOME/$FILE.bak"
 	fi
 
 	echo "$FILE => $HOME/$FILE"
-	ln -sr "$FILE" "$HOME/$FILE"
-done
-
-# symlink dirs
-for DIR in $DIRS; do
-
-	# if dir is a symlink, remove it and later make a new symlink
-	if [ -L "$HOME/$DIR" ]; then
-		rm -r "$HOME/$DIR"
-	# elif dir already exists but is not backed up, make a backup
-	elif [ -d "$HOME/$DIR" -a ! -d "$HOME/$DIR.bak" ]; then
-		echo "backed up $HOME/$DIR => $HOME/$DIR.bak"
-		mv "$HOME/$DIR" "$HOME/$DIR.bak"
-	fi
-
-	echo "$DIR => $HOME/$DIR"
-	ln -sr "$DIR" "$HOME/$DIR"
+	$DRYRUN ln -sr "$FILE" "$HOME/$FILE"
 done
 
 echo "all done!"
