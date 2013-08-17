a47762037e0a42ac6626f6d9ac724686488c7b6b
Rasmus Eskola <fruitiex@gmail.com>
2013-08-17 23:13:42 +0300

    minor updates

--- bin/wallpaper.sh
+++ bin/wallpaper.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 while true; do
-	find ~/pics/wallpapers -type f \( -name '*.jpg' -o -name '*.png' -o -name '*.jpeg' -o -name '*.gif' \) -print0 | shuf -n1 -z | xargs -0 feh --bg-fill
+	find ~/images/wallpapers -type l \( -name '*.jpg' -o -name '*.png' -o -name '*.jpeg' -o -name '*.gif' \) -print0 | shuf -n1 -z | xargs -0 feh --bg-fill
 	if [[ $1 == "-o" ]]
 	then
 		exit $?
