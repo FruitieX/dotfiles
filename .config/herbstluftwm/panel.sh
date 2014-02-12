87f487da9b0d6fe962edc62a5aabc4fe8859d8f8
Rasmus Eskola <fruitiex@gmail.com>
2014-02-12 19:05:29 +0200

    status bar: bigger font

--- .config/herbstluftwm/panel.sh
+++ .config/herbstluftwm/panel.sh
@@ -11,11 +11,11 @@ fi
 x=${geometry[0]}
 y=${geometry[1]}
 panel_width=${geometry[2]}
-panel_height=16
-font="-*-terminus-medium-*-*-*-12-*-*-*-*-*-*-*"
+panel_height=20
+font="-*-terminus-medium-*-*-*-14-*-*-*-*-*-*-*"
 bgcolor=$(hc get frame_border_normal_color)
 selbg=$(hc get window_border_active_color)
-selfg='#101010'
+selfg='#080808'
 
 ####
 # Try to find textwidth binary.
