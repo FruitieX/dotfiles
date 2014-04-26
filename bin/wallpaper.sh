5b9d9082feb1e8ab9977a3b26fa4fa122bc6fd3c
Rasmus Eskola <fruitiex@gmail.com>
2014-04-27 02:01:57 +0300

    update wallpaper path

--- bin/wallpaper.sh
+++ bin/wallpaper.sh
@@ -2,7 +2,7 @@
 
 cd ~
 while true; do
-	wallpaper="$(find annex/images/wallpapers -type l \( -name '*.jpg' -o -name '*.png' -o -name '*.jpeg' -o -name '*.gif' \) -print0 | shuf -n1 -z | xargs -0 echo)"
+	wallpaper="$(find /btrfs/annex/images/wallpapers -type l \( -name '*.jpg' -o -name '*.png' -o -name '*.jpeg' -o -name '*.gif' \) -print0 | shuf -n1 -z | xargs -0 echo)"
 	echo $wallpaper
 	if [[ $HOST == "lappy" || $HOST == "bulky" ]]; then
 		ssh -o ConnectTimeout=10 -o BatchMode=yes ircscreen@fruitiex.org DISPLAY=:0 feh --bg-fill \"$wallpaper\"
