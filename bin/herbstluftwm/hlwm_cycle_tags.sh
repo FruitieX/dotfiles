f604e6764fa25e5e2bb680e4d10056f750baa124
Rasmus Eskola <fruitiex@gmail.com>
2014-06-03 15:59:42 +0300

    quick hack for if an urgent tag is on another monitor; cycle would fail

--- bin/herbstluftwm/hlwm_cycle_tags.sh
+++ bin/herbstluftwm/hlwm_cycle_tags.sh
@@ -10,6 +10,11 @@ if [[ $1 -gt 0 ]]; then
 	for (( i = $active_tag + 1 ; i < $active_tag + ${#tags[@]} ; i += $1 )) do
 		current_tag=$((i % ${#tags[@]}))
 		name=${tags[$current_tag]}
+		hc stack | grep "Monitor .* with tag \"${name:1}\"" > /dev/null
+		ret=$?
+		if [[ $ret == 0 ]]; then
+			continue;
+		fi
 		if [[ $name == !* || $name == :* ]]; then
 			hc use ${name:1}
 			break;
@@ -19,6 +24,11 @@ else
 	for (( i = $active_tag - 1 ; i > $active_tag - ${#tags[@]} ; i += $1 )) do
 		current_tag=$((i % ${#tags[@]}))
 		name=${tags[$current_tag]}
+		hc stack | grep "Monitor .* with tag \"${name:1}\"" > /dev/null
+		ret=$?
+		if [[ $ret == 0 ]]; then
+			continue;
+		fi
 		if [[ $name == !* || $name == :* ]]; then
 			hc use ${name:1}
 			break;
