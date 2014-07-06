3ee7896e614caf78d4b58a2e74c657154230d74c
Rasmus Eskola <fruitiex@gmail.com>
2014-07-07 00:19:39 +0300

    appropriate disclaimer in firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -1,5 +1,19 @@
 #!/bin/zsh
 #
+#
+echo "WARNING: This script does a LOT of magic to your home directory!"
+echo "If you haven't done so yet, go and read through it NOW!"
+echo "You *probably* don't want to run it unless you want a set up exactly like mine"
+echo "In other cases, for your own safety, just manually symlink any config files you want to your home directory"
+echo "I will provide little to no support if you break your system with this :)"
+echo
+echo "are you sure you want to continue? (y/n)"
+read answer
+if [[ "$answer" != "y" ]]; then
+	echo "aborting."
+	exit
+fi
+
 # cd to root of git repo
 cd "$(dirname $0)"
 CWD=$(pwd)
