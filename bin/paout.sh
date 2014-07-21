a2f1fdddde7561a24723d9f56ba39842d545b918
Rasmus Eskola <fruitiex@gmail.com>
2014-07-21 13:28:40 +0300

    add comment

--- bin/paout.sh
+++ bin/paout.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-REQUESTED_LATENCY=100
+# play audio output from remote pulseaudio server locally
 
-until PULSE_SERVER=10.8.0.1 pacat -r --device=alsa_output.usb-Burr-Brown_from_TI_USB_Audio_CODEC-00-CODEC.analog-stereo.monitor --latency-msec=$REQUESTED_LATENCY | pacat -p --latency-msec=$REQUESTED_LATENCY
+REQUESTED_LATENCY=100
+DEV=alsa_output.usb-Burr-Brown_from_TI_USB_Audio_CODEC-00-CODEC.analog-stereo.monitor
+
+until PULSE_SERVER=10.8.0.1 pacat -r --device=$DEV --latency-msec=$REQUESTED_LATENCY | pacat -p --latency-msec=$REQUESTED_LATENCY
 do
     echo reconnecting to pulseaudio server...
     sleep 1
