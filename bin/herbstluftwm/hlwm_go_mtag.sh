ad4d7c74a2d671c230eba5549a3da752bac87d68
Rasmus Eskola <fruitiex@gmail.com>
2014-10-12 22:50:34 +0300

    hlwm tag keys now refer to monitor

---
+++ bin/herbstluftwm/hlwm_go_mtag.sh
@@ -0,0 +1,12 @@
+hc() {
+	herbstclient "$@"
+}
+
+target_tag=$(hc get_attr monitors.by-name.monitor${1}.tag)
+if [[ $? != 0 ]]; then
+    echo Error: invalid monitor id, quitting
+    exit 1
+fi
+
+echo $target_tag
+hc use $target_tag
