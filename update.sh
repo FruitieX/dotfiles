dcd6f0efe81e563415d5670c5c5e777bc02fd522
Rasmus Eskola <fruitiex@gmail.com>
2014-12-26 15:12:31 +0200

    submodule bump & fix update script to add upstream repo of .zprezto

--- update.sh
+++ update.sh
@@ -7,14 +7,15 @@ cd "$(dirname $0)"
 CWD=$(pwd)
 
 cd .zprezto
-git checkout master
+git remote add upstream https://github.com/sorin-ionescu/prezto || true 2>/dev/null
 git pull upstream master
+git checkout master
 cd "$CWD"
 
 git pull
 git submodule sync
 git submodule update --init --recursive
-git submodule foreach --recursive git checkout master
 git submodule foreach --recursive git pull origin master
+git submodule foreach --recursive git checkout master
 
 ./symlink.sh -y
