b22edd1802c18d6804a97288e4c884b36ea6a0b2
Rasmus Eskola <fruitiex@gmail.com>
2016-01-14 18:50:40 +0200

    steamroute.sh update to ip tools

--- bin/steamroute.sh
+++ bin/steamroute.sh
@@ -1,1 +1,2 @@
-sudo route add -net 255.255.255.255 netmask 255.255.255.255 dev tap0 metric 0
+#sudo route add -net 255.255.255.255 netmask 255.255.255.255 dev tap0 metric 0
+sudo ip route add 255.255.255.255 via 10.9.8.100 dev tap0
